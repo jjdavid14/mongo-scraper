894584c4bdce6eb679e90fc8732b3fdb6fd675c7
JamieDavid <jjdavid14@live.com>
2018-03-20 17:25:03 -0700

    deploy

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.set('view engine', 'handlebars');
 
 // Database Configuration with Mongoose
 // Connect to localhost if not a production environment
-if(process.env.NODE_ENV == 'production'){
-  //mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
+if(process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
 }
 else{
-  mongoose.connect('mongodb://localhost/news-scraper');
+  mongoose.connect('mongodb://localhost/sports-scrape');
 }
 var db = mongoose.connection;
 
